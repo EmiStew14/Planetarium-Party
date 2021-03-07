47b2ec6526bfd0195818f2002f5a918c5f85c65f
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-07 16:52:03 -0700

    added comments to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//universal variables
+//function for rendering night mode ((more on the css side))
+//storage of birthdates in this format MM/DD/YYYY
+//loading of birthdates
+//APIs for song titles
+//generate music videos or album covers
+//APIs for Movie titles
+//display movie posters
+
