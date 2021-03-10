f92450aa8a9c60bcb133be6d9b5a1b1636305697
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-03-09 18:27:23 -0700

    api setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,28 @@
 //function for rendering night mode ((more on the css side))
 //storage of birthdates in this format MM/DD/YYYY
 //loading of birthdates
-//APIs for song titles
+//APIs for song titles api key https://api.musixmatch.com/ws/1.1/  68d7dd5005e9eaf38415e4045967649a
+fetch(
+  "https://api.musixmatch.com/ws/1.1/track_id?api_key=68d7dd5005e9eaf38415e4045967649a",
+  { mode: "no-cors" }
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+//   fetch(
+//     'https://api.giphy.com/v1/gifs/trending?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'
+//   )
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(data) {
+//       console.log(data);
+//     });
+//     , { mode: 'no-cors'}
 //generate music videos or album covers
 //APIs for Movie titles
 //display movie posters
-
