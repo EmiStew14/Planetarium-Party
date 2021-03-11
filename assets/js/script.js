2fd3018c67832b14ce0116acd525d1d8a4495ec7
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-10 18:53:20 -0700

    added card blocks and some JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
+import "tailwindcss/tailwind.css";
 //universal variables
 //function for rendering night mode ((more on the css side))
 //storage of birthdates in this format MM/DD/YYYY
+$(birthdata).on("click",(){
+    hideForm();
+})
+
+function hideForm () {
+    document.getElementById("bday-banner").setAttribute("style","display:none");
+}
 //loading of birthdates
 //APIs for song titles
 //generate music videos or album covers
