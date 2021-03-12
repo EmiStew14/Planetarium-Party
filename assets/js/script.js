101adf59110c6b69543b27bf62d6f240d700ffc3
maribels_are_ringing <maribel.duron86@gmail.com>
2021-03-11 19:04:47 -0700

    horoscope api JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,22 @@
 //loading of birthdates
 //APIs for song titles
 //generate music videos or album covers
-//APIs for Movie titles
-//display movie posters
 
+//APIs for Horoscope
+fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_finder/result/", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"x-rapidapi-key": "30d55d58d4mshd24355021b03dbap19337fjsn5e97d59eee51",
+		"x-rapidapi-host": "astrology-horoscope.p.rapidapi.com"
+	},
+	"body": {
+		"mystic_dob": "1988-05-11"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
