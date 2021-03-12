b036f7daf36e40b939e3bd00f5907cc4696e554e
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-03-11 19:02:02 -0700

    add java

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,22 @@
 //storage of birthdates in this format MM/DD/YYYY
 //loading of birthdates
 //APIs for song titles api key https://api.musixmatch.com/ws/1.1/  68d7dd5005e9eaf38415e4045967649a
-fetch(
-  "https://api.musixmatch.com/ws/1.1/track_id?api_key=68d7dd5005e9eaf38415e4045967649a",
-  { mode: "no-cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+//spotify api https://api.spotify.com client id-48a673c66fbc4f6eb2efc416c3f45300
 
-//   fetch(
-//     'https://api.giphy.com/v1/gifs/trending?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'
-//   )
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(data) {
-//       console.log(data);
-//     });
-//     , { mode: 'no-cors'}
+fetch("https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa", {
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-key": "a040ae462d3a4987b352f4b4689f5141",
+		"x-rapidapi-host": "48a673c66fbc4f6eb2efc416c3f45300"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
 //generate music videos or album covers
 //APIs for Movie titles
 //display movie posters
