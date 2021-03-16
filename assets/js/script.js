6cb25baaf71b8ae8d224c885cbd7dbbd1409a47d
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-15 19:41:40 -0700

    corrected syntax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,91 @@
 
+function wikiSearch(searchTerm) {
+    //Api for Wiki article
+    // fetch("https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=" + searchTerm)
+    fetch("https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles=" + searchTerm)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        var wikiCard = $("<div>").addClass("card-wiki");
+        //var planetTitle = $("<h1>").addClass("card-title").text(data.query.search[0].title);
+        $("#response-wiki").each(function(){
+            if (searchTerm.toLowercase() = "pluto"){
+                var plutoCardBody = $("<div>").addClass("card-body");
+                var plutoArticle = $("<p>").addClass("article-Text").text(data.query.pages[44469].extract);
+                console.log(data.query.pages[44469].extract);
+                plutoCardBody.append(plutoArticle);
+                wikiCard.append(plutoCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+            if (searchTerm === "Neptune"|| "neptune" ){
+                var neptuneCardBody = $("<div>").addClass("card-body");
+                var neptuneArticle = $("<p>").addClass("article-Text").text(data.query.pages[19003265].extract);
+                console.log(data.query.pages[19003265].extract);
+                neptuneCardBody.append(neptuneArticle);
+                wikiCard.append(neptuneCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+            if (searchTerm === "Jupiter"|| "jupiter" ){
+                var planetCardBody = $("<div>").addClass("card-body");
+                var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[38930].extract);
+                planetCardBody.append(planetArticle);
+                wikiCard.append(planetCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+            if (searchTerm === "Saturn"|| "saturn" ){
+                var planetCardBody = $("<div>").addClass("card-body");
+                var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[44474].extract);
+                planetCardBody.append(planetArticle);
+                wikiCard.append(planetCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+            if (searchTerm === "Venus"|| "venus" ){
+                var planetCardBody = $("<div>").addClass("card-body");
+                var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[32745].extract);
+                planetCardBody.append(planetArticle);
+                wikiCard.append(planetCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+            if (searchTerm === "Mercury"|| "mercury" ){
+                var planetCardBody = $("<div>").addClass("card-body");
+                var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[19007].extract);
+                planetCardBody.append(planetArticle);
+                wikiCard.append(planetCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+            if (searchTerm === "Uranus"|| "uranus" ){
+                var planetCardBody = $("<div>").addClass("card-body");
+                var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[44475].extract);
+                planetCardBody.append(planetArticle);
+                wikiCard.append(planetCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+            if (searchTerm === "Mars"|| "mars" ){
+                var planetCardBody = $("<div>").addClass("card-body");
+                var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[14640471].extract);
+                planetCardBody.append(planetArticle);
+                wikiCard.append(planetCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+            if (searchTerm === "Earth"|| "earth" ){
+                var planetCardBody = $("<div>").addClass("card-body");
+                var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[9228].extract);
+                planetCardBody.append(planetArticle);
+                wikiCard.append(planetCardBody);
+                $("#response-wiki").append(wikiCard);
+            }
+        })
+        // var planetCardBody = $("<div>").addClass("card-body");
+         // var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[44474].extract);
+        //  planetCardBody.append(planetArticle);
+        //  wikiCard.append(planetCardBody);
+        //  $("#response-wiki").append(wikiCard);
+
+    });
+    //jupiter 38930 , saturn 44474 , neptune 19003265 , venus 32745 , uranus 44475 , mars 14640471 , mercury 19007 , earth 9228 , pluto 44469
+}
 
 function myFunction() {
 var searchTerm = document.querySelector('#searchTerm').value;
@@ -17,7 +104,7 @@ var images = {};
         var spaceImg = document.createElement('img');
         seePic.appendChild(spaceImg);
          spaceImg.setAttribute('src', data.collection.items[9].links[0].href);
-         document.querySelector('#text-banner').appendChild(seePic);
+         document.querySelector('#response-Nasa').appendChild(seePic);
         console.log(data.collection.items[0].links[0].href);
 		
 		wikiSearch(searchTerm);
@@ -28,92 +115,7 @@ var images = {};
 	
     }
 
-	function wikiSearch(searchTerm) {
-		//Api for Wiki article
-		// fetch("https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=" + searchTerm)
-        fetch("https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles=" + searchTerm)
-		.then(function(response) {
-			return response.json();
-		})
-		.then(function(data) {
-			console.log(data);
-			var wikiCard = $("<div>").addClass("card-wiki");
-			//var planetTitle = $("<h1>").addClass("card-title").text(data.query.search[0].title);
-            $("#response-wiki").each(function(){
-                if (searchTerm === "Pluto"|| "pluto" ){
-                    var plutoCardBody = $("<div>").addClass("card-body");
-                    var plutoArticle = $("<p>").addClass("article-Text").text(data.query.pages[44469].extract);
-                    plutoCardBody.append(plutoArticle);
-                    wikiCard.append(plutoCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-                if (searchTerm === "Neptune"|| "neptune" ){
-                    var neptuneCardBody = $("<div>").addClass("card-body");
-                    var neptuneArticle = $("<p>").addClass("article-Text").text(data.query.pages[19003265].extract);
-                    console.log(data.query.pages[19003265].extract);
-                    neptuneCardBody.append(neptuneArticle);
-                    wikiCard.append(neptuneCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-                if (searchTerm === "Jupiter"|| "jupiter" ){
-                    var planetCardBody = $("<div>").addClass("card-body");
-                    var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[38930].extract);
-                    planetCardBody.append(planetArticle);
-                    wikiCard.append(planetCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-                if (searchTerm === "Saturn"|| "saturn" ){
-                    var planetCardBody = $("<div>").addClass("card-body");
-                    var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[44474].extract);
-                    planetCardBody.append(planetArticle);
-                    wikiCard.append(planetCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-                if (searchTerm === "Venus"|| "venus" ){
-                    var planetCardBody = $("<div>").addClass("card-body");
-                    var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[32745].extract);
-                    planetCardBody.append(planetArticle);
-                    wikiCard.append(planetCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-                if (searchTerm === "Mercury"|| "mercury" ){
-                    var planetCardBody = $("<div>").addClass("card-body");
-                    var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[19007].extract);
-                    planetCardBody.append(planetArticle);
-                    wikiCard.append(planetCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-                if (searchTerm === "Uranus"|| "uranus" ){
-                    var planetCardBody = $("<div>").addClass("card-body");
-                    var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[44475].extract);
-                    planetCardBody.append(planetArticle);
-                    wikiCard.append(planetCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-                if (searchTerm === "Mars"|| "mars" ){
-                    var planetCardBody = $("<div>").addClass("card-body");
-                    var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[14640471].extract);
-                    planetCardBody.append(planetArticle);
-                    wikiCard.append(planetCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-                if (searchTerm === "Earth"|| "earth" ){
-                    var planetCardBody = $("<div>").addClass("card-body");
-                    var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[9228].extract);
-                    planetCardBody.append(planetArticle);
-                    wikiCard.append(planetCardBody);
-                    $("#response-wiki").append(wikiCard);
-                }
-            })
-			// var planetCardBody = $("<div>").addClass("card-body");
-     		// var planetArticle = $("<p>").addClass("article-Text").text(data.query.pages[44474].extract);
-			//  planetCardBody.append(planetArticle);
-			//  wikiCard.append(planetCardBody);
-			//  $("#response-wiki").append(wikiCard);
-
-		});
-		//jupiter 38930 , saturn 44474 , neptune 19003265 , venus 32745 , uranus 44475 , mars 14640471 , mercury 19007 , earth 9228 , pluto 44469
-	}
+	
 	
       
       
@@ -175,10 +177,6 @@ var images = {};
     });
 
     $("#space").click(function(){});
-
-function getArt() {
-
-};
 //function for rendering night mode ((more on the css side))
 //generate music videos or album covers
 //display movie posters
